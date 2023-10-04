60fe51c4d89dec1841364170b5308d390413bcfb
rtsoftgondia <gondia.rtsoft@gmail.com>
2023-10-04 12:02:20 +0530

    Control Structure and Operators

---
+++ JavaScript Basic/Product Discount.js
@@ -0,0 +1,14 @@
+let productPrice = 11000;
+
+let discount = 0;
+let totalPay = 0;
+
+if(productPrice > 10000) discount = productPrice * 0.3;
+else if(productPrice > 8000) discount = productPrice * 0.2;
+else if(productPrice > 5000) discount = productPrice * 0.15;
+else if(productPrice > 3000) discount = productPrice * 0.1;
+else if(productPrice > 2000) discount = productPrice * 0.05;
+else discount = 0;
+
+totalPay = productPrice - discount;
+console.log("Total Amount to be paid : " + totalPay + " Rupees Only.");
---
+++ JavaScript Basic/Swapping of Two Variables.js
@@ -0,0 +1,14 @@
+let x = 10;
+let y = 20;
+
+console.log("Before Interchange ")
+console.log(" x = " + x )
+console.log(" y = " + y )
+
+let temp = x;
+x = y;
+y = temp;
+
+console.log("After Interchange ")
+console.log(" x = " + x )
+console.log(" y = " + y )
